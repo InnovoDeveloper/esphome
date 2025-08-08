a0675ef3f87b47b96347622c9b1efd98e3f388fc
ZipperSpark <65334023+InnovoDeveloper@users.noreply.github.com>
2025-08-07 17:11:39 -0700

    Update sonic_i2c.h

--- components/sonic_i2c/sonic_i2c.h
+++ components/sonic_i2c/sonic_i2c.h
@@ -5,14 +5,14 @@
 #include "esphome/components/i2c/i2c.h"
 #include "esphome/components/sensor/sensor.h"
 
-namespace esphome{
-namespace sonic_i2c_sensor{
- 
+namespace esphome {
+namespace sonic_i2c {
+
 class SonicI2C: public i2c::I2CDevice, public sensor::Sensor, public PollingComponent {
     public:
-     void setup() override ;
+     void setup() override;
      void dump_config() override;
-     void update() override ;
+     void update() override;
      void start_measurement();
      void read_distance();
 
@@ -21,6 +21,5 @@ class SonicI2C: public i2c::I2CDevice, public sensor::Sensor, public PollingComp
      uint32_t measurement_start_time_{0};
 };
 
-}
-
-}
+}  // namespace sonic_i2c
+}  // namespace esphome
