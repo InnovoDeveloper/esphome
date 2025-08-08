222411fe3d2e30a8447f586f08c2e036847b8af3
ZipperSpark <65334023+InnovoDeveloper@users.noreply.github.com>
2025-08-07 17:38:46 -0700

    Update __init__.py

--- components/sonic_i2c/__init__.py
+++ components/sonic_i2c/__init__.py
@@ -1,1 +1,33 @@
-#minimal
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import sensor, i2c
+from esphome.const import (
+    CONF_ID,
+    STATE_CLASS_MEASUREMENT,
+    UNIT_METER,
+    ICON_ARROW_EXPAND_VERTICAL,
+)
+
+MULTI_CONF = True
+DEPENDENCIES = ['i2c']
+AUTO_LOAD = ["sensor"]
+
+sonic_sensor_ns = cg.esphome_ns.namespace("sonic_i2c_sensor")
+SonicI2C = sonic_sensor_ns.class_("SonicI2C", sensor.Sensor, i2c.I2CDevice, cg.PollingComponent)
+
+CONFIG_SCHEMA = (
+    sensor.sensor_schema(
+        SonicI2C,
+        unit_of_measurement=UNIT_METER,
+        icon=ICON_ARROW_EXPAND_VERTICAL,
+        accuracy_decimals=2,
+        state_class=STATE_CLASS_MEASUREMENT,
+    )
+    .extend(cv.polling_component_schema("60s")
+    .extend(i2c.i2c_device_schema(0x57)))
+)
+
+async def to_code(config):
+    var = await sensor.new_sensor(config)
+    await cg.register_component(var, config)
+    await i2c.register_i2c_device(var, config)
