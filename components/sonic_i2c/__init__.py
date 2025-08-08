00506bd10044e32e28b7fec669e3cab9854c17cd
ZipperSpark <65334023+InnovoDeveloper@users.noreply.github.com>
2025-08-07 17:51:16 -0700

    Update __init__.py

--- components/sonic_i2c/__init__.py
+++ components/sonic_i2c/__init__.py
@@ -2,7 +2,6 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor, i2c
 from esphome.const import (
-    CONF_ID,
     STATE_CLASS_MEASUREMENT,
     UNIT_METER,
     ICON_ARROW_EXPAND_VERTICAL,
